2e0360888b867af11c1753d9e90d7a0161f96fa1
Kieran Mace <kieran.mace@gmail.com>
2017-03-21 10:21:39 -0700

    Fixed column checking of plate dataframe

--- R/summarize-growth-by-plate.R
+++ R/summarize-growth-by-plate.R
@@ -89,7 +89,7 @@ SummarizeGrowthByPlate <- function(plate,
   }
 
   # make sure that there is a column named "time" in the input
-  if (sum(grep("time", names(plate), ignore.case = TRUE)) != 1) {
+  if (length(grep("time", names(plate), ignore.case = TRUE)) != 1) {
     stop("There must be exactly one column named 'time' in the 'plate' data.frame.",
          call. = FALSE)
   }
@@ -103,7 +103,7 @@ SummarizeGrowthByPlate <- function(plate,
 
   if (bg_correct == "blank") {
     # check that there is a column in the plate data.frame containing the blanks
-    if (sum(grep("blank", names(plate), ignore.case = TRUE)) != 1) {
+    if (length(grep("blank", names(plate), ignore.case = TRUE)) != 1) {
       stop("There must be exactly one column named 'blank' in the 'plate' data.frame if you have selected the bg_correct 'plate' option.",
            call. = FALSE)
     }
